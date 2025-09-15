25fb1541f24132f6174def151fa519294e914099
H11iye <hamdi.iyed11@gmail.com>
2025-09-15 10:05:24 +0000

    Add an entrypoint + modify the port + ignore the docker compose file

--- app/main.py
+++ app/main.py
@@ -41,4 +41,6 @@ def get_weather():
     }
     return jsonify(weather_info)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500)
+    import os
+    port = int(os.environ.get("PORT", 8080)) #Default to 8080 of PORT is not set 
+    app.run(host='0.0.0.0', port=port)
